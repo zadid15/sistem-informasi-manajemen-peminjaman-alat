7c12033f8656e9cf439a39c225b8d585f9c0bd5b
zadid15 <muhammadzadid061512@gmail.com>
2026-04-07 09:25:13 +0700

    edit axios

--- fe/src/utils/axios.ts
+++ fe/src/utils/axios.ts
@@ -4,21 +4,17 @@ const axiosInstance = axios.create({
     baseURL: "https://almeda-noctis-nonpastorally.ngrok-free.dev/api",
     headers: {
         "ngrok-skip-browser-warning": "true"
-    },
-    withCredentials: true // aman kalau nanti pakai auth cookie
+    }
 });
 
-axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+    return config;
+});
 
 export default axiosInstance;
